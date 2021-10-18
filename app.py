e737739e7629aee7fe9f2471b9f77842cce4b4c7
Zach <Zacaframe@gmail.com>
2021-10-18 08:31:25 -0400

    Board complete

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask_debugtoolbar import DebugToolbarExtension
+from flask import Flask, render_template, request, redirect, flash, session
+from boggle import Boggle
+
+boggle_game = Boggle()
+BOARD = "board"
+app = Flask(__name__)
+app.debug = True
+app.config['SECRET_KEY'] = "secret"
+
+debug = DebugToolbarExtension(app)
+
+boggleBoard = boggle_game.make_board()
+
+@app.route("/")
+def generate_home():
+    """Home page. Generate Board."""
+
+    session[BOARD] = boggleBoard
+    board = session.get(BOARD)
+
+    return render_template("home.html", board=board)
+
+@app.route("/guess", methods=["POST"])
+def make_guess():
+
+    return redirect("/")
